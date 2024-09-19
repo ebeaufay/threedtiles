8dec4dcd37e19369dec129e1983d503cdefccaf0
Emeric Beaufays <emericbeaufays@gmail.com>
2024-09-19 11:31:07 +0200

    export getOGC3DTilesCopyrightInfo function

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,5 @@
 export { OcclusionCullingService } from './tileset/OcclusionCullingService';
-export { OGC3DTile } from "./tileset/OGC3DTile";
+export { OGC3DTile, getOGC3DTilesCopyrightInfo } from "./tileset/OGC3DTile";
 export { TileLoader } from "./tileset/TileLoader";
 export { InstancedOGC3DTile } from "./tileset/instanced/InstancedOGC3DTile.js"
 export { InstancedTileLoader } from "./tileset/instanced/InstancedTileLoader.js"
